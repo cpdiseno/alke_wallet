94f45d75fd7549aa47ef8b31666ad723fd537ed6
Camila Peña <camiladiseno@gmail.com>
2024-05-04 15:31:50 -0400

    Actualizaciòn de pantallas home, request, signup y empty

---
+++ app/src/main/java/pena/camila/alkewallet/fragments/request_money.java
@@ -0,0 +1,29 @@
+package pena.camila.alkewallet.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pena.camila.alkewallet.R;
+
+public class request_money extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_request_money, container, false);
+    }
+}
